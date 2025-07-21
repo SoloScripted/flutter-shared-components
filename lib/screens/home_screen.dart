4c4a3c88909d096f6443504ddafa093aba41d73e
Mustafa Kara <mkara@soloscripted.com>
2025-07-21 22:12:38 +0300

    feat:Generate components and widgets (#1)
    * feat:Generate components and widgets
    
    Signed-off-by: Mustafa Kara <mkara@soloscripted.com>
    
    * feat:Generate components and widgets
    
    Signed-off-by: Mustafa Kara <mkara@soloscripted.com>
    
    ---------
    
    Signed-off-by: Mustafa Kara <mkara@soloscripted.com>

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key, required this.nextScreen});
+
+  final Widget nextScreen;
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late AnimationController _fadeOutController;
+
+  late Animation<double> _fadeOutAnimation;
+
+  late Animation<Offset> _soloAnimation;
+  late Animation<Offset> _scriptedAnimation;
+  late Animation<double> _companyNameFadeAnimation;
+  late Animation<double> _logoFadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 2500),
+    );
+
+    _fadeOutController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    );
+
+    _logoFadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: const Interval(0.0, 0.6, curve: Curves.easeIn),
+      ),
+    );
+
+    _companyNameFadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: const Interval(0.6, 1.0, curve: Curves.easeIn),
+      ),
+    );
+
+    _soloAnimation =
+        Tween<Offset>(begin: const Offset(-1.5, 0), end: Offset.zero).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: const Interval(0.6, 1.0, curve: Curves.easeOut),
+          ),
+        );
+    _scriptedAnimation =
+        Tween<Offset>(begin: const Offset(1.5, 0), end: Offset.zero).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: const Interval(0.6, 1.0, curve: Curves.easeOut),
+          ),
+        );
+
+    _fadeOutAnimation = Tween<double>(begin: 1.0, end: 0.0).animate(
+      CurvedAnimation(parent: _fadeOutController, curve: Curves.easeIn),
+    );
+
+    _animationController.forward().whenComplete(() {
+      _fadeOutController.forward().whenComplete(() {
+        if (mounted) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => widget.nextScreen),
+          );
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    _fadeOutController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const baseTextStyle = TextStyle(fontSize: 36, fontWeight: FontWeight.bold);
+
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      body: FadeTransition(
+        opacity: _fadeOutAnimation,
+        child: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      FadeTransition(
+                        opacity: _logoFadeAnimation,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(24.0),
+                          child: Image.asset(
+                            'assets/logo.png',
+                            height: 160,
+                            errorBuilder: (context, error, stackTrace) {
+                              return const Icon(
+                                Icons.code_sharp,
+                                size: 160,
+                                color: Colors.grey,
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      FadeTransition(
+                        opacity: _companyNameFadeAnimation,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SlideTransition(
+                              position: _soloAnimation,
+                              child: Text(
+                                'Solo',
+                                style: baseTextStyle.copyWith(
+                                  color: Colors.blue,
+                                ),
+                              ),
+                            ),
+                            SlideTransition(
+                              position: _scriptedAnimation,
+                              child: Text(
+                                'Scripted',
+                                style: baseTextStyle.copyWith(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 20.0),
+                child: Text(
+                  "© 2025 SoloScripted",
+                  style: TextStyle(color: Colors.grey[600], fontSize: 14),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
