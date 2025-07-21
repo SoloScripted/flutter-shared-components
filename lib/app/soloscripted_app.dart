e146bba2127aa50ac43ca3eee3c8a31589040594
Mustafa Kara <mkara@soloscripted.com>
2025-07-21 22:29:12 +0300

    create shared app (#2)
    Signed-off-by: Mustafa Kara <mkara@soloscripted.com>

---
+++ lib/app/soloscripted_app.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shared_components/flutter_shared_components.dart';
+
+class SoloScriptedApp extends StatelessWidget {
+  const SoloScriptedApp({
+    super.key,
+    required title,
+    required localizationsDelegates,
+    required supportedLocales,
+    required mainScreen
+  });
+
+  final String title;
+  final List<LocalizationsDelegate<dynamic>> localizationsDelegates;
+  final List<Locale> supportedLocales;
+  final Widget mainScreen;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:title,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        useMaterial3: true,
+      ),
+      localizationsDelegates: localizationsDelegates,
+      supportedLocales: supportedLocales,
+      home: HomeScreen(
+        nextScreen: mainScreen,
+      ),
+    );
+  }
+}
