a278a191d813cce466133b12944b0b68cbdb89c3
Mustafa Kara <mkara@soloscripted.com>
2025-08-08 06:33:17 +0300

    feat: Request App Tracking Transparency authorization on launch (#6)
    * feat: Request App Tracking Transparency authorization on launch
    
    Implements the App Tracking Transparency (ATT) framework to request user authorization for accessing the device's advertising identifier (IDFA) on iOS.
    
    This change introduces the app_tracking_transparency package and converts the SoloScriptedApp widget into a StatefulWidget. This allows for the authorization request to be triggered within the initState method, ensuring that the permission dialog is presented to the user when the application first initializes.
    
    The request is only made if the authorization status is notDetermined, preventing repeated prompts to the user. This is a necessary step for compliance with Apple's privacy policies on iOS 14.5 and later.
    
    Signed-off-by: Mustafa Kara <mkara@soloscripted.com>
    
    * fix:dependencies
    
    Signed-off-by: Mustafa Kara <mkara@soloscripted.com>
    
    * fix:formatting
    
    Signed-off-by: Mustafa Kara <mkara@soloscripted.com>
    
    * fix:formatting
    
    Signed-off-by: Mustafa Kara <mkara@soloscripted.com>
    
    ---------
    
    Signed-off-by: Mustafa Kara <mkara@soloscripted.com>

--- lib/app/soloscripted_app.dart
+++ lib/app/soloscripted_app.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shared_components/screens/home_screen.dart';
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 
-class SoloScriptedApp extends StatelessWidget {
+class SoloScriptedApp extends StatefulWidget {
   const SoloScriptedApp(
       {super.key,
       required this.title,
@@ -10,22 +11,50 @@ class SoloScriptedApp extends StatelessWidget {
       required this.mainScreen});
 
   final String title;
+
   final List<LocalizationsDelegate<dynamic>> localizationsDelegates;
+
   final List<Locale> supportedLocales;
+
   final Widget mainScreen;
+
+  @override
+  State<SoloScriptedApp> createState() => _SoloScriptedAppState();
+}
+
+class _SoloScriptedAppState extends State<SoloScriptedApp> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _requestTrackingAuthorization();
+    });
+  }
+
+  Future<void> _requestTrackingAuthorization() async {
+    try {
+      final status = await AppTrackingTransparency.trackingAuthorizationStatus;
+      if (status == TrackingStatus.notDetermined) {
+        await AppTrackingTransparency.requestTrackingAuthorization();
+      }
+    } catch (e) {
+      // Silently ignore if there's an error during the request, as the
+      // app should continue to function without this permission.
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: title,
+      title: widget.title,
       theme: ThemeData(
         colorSchemeSeed: Colors.blue,
         useMaterial3: true,
       ),
-      localizationsDelegates: localizationsDelegates,
-      supportedLocales: supportedLocales,
+      localizationsDelegates: widget.localizationsDelegates,
+      supportedLocales: widget.supportedLocales,
       home: HomeScreen(
-        nextScreen: mainScreen,
+        nextScreen: widget.mainScreen,
       ),
     );
   }
